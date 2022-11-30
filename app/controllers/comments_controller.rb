7bd2b3aa00f550f3ef9de2fbdc4556aae015c429
raphael <raphael.bichet@yahoo.com>
2022-11-30 19:21:28 +0100

    ajout dans le controller des methode index, new, create et private + les pages dans views pour index et new

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,31 @@
 class CommentsController < ApplicationController
+
+  def index
+    @post = Post.find(params[:post_id])
+    @comments = Comment.where(post: @post)
+  end
+
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment.post = @post
+    if @comment.save
+      redirect_to post_comments_path(), notice: "Comment has been added"
+    else
+      render :new
+    end
+  end
+
+private
+
+def comment_params
+  params.require(:comment).permit(:avis)
+end
+
+
 end
